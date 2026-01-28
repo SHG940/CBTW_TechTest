8d5d5de5b224be243382760151f5296d1e2e66d1
Santiago <santiago.9405@gmail.com>
2026-01-27 19:07:12 -0500

    Connection between API and Gateway

--- CBTW.API/Controllers/GeminiController.cs
+++ CBTW.API/Controllers/GeminiController.cs
@@ -14,20 +14,18 @@ public class GeminiController : ControllerBase
     public GeminiController(IAiService aiService)
         => this.aiService = aiService;
 
-    [HttpPost]
+    [HttpPost("GetBookInfo")]
     public async Task<IActionResult> Post(GetAiAnalysisRequest request)
     {
-        const string basePrompt = "Extract the key information of the following input related to a book; " +
-                                  "the key information I want you to extract is the following: author, title, and keywords." +
-                                  "The way I want you to give me the information is in minimized JSON format as the following example (the json object only):" +
-                                  "{'title':'Moby Dick','author':'Herman Melville','keywords':['Adventure Fiction','1851-10-18','Whale','Sea','Ahab Captain']}." +
-                                  "In case that you can't find the title or the author, you must assign the field as null" +
-                                  "Input: {{text}}";
         var result = await aiService.CallAsync(new AIServiceRequest
         {
-            Propmt = basePrompt,
             PropmtValues = new Dictionary<string, string> { {"text", request.Text} }
         });
+
+        if (result is null || result.Result == "not found")
+        {
+            return BadRequest();
+        }
         
         return Ok(result);
     }
